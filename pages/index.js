698ee5c225b12636f345f5fd7a73374c873fa7c6
dejan1stefanov <dejan1stefanov@gmail.com>
2022-09-26 16:20:30 +0200

    Finished project

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+export default function Home({ posts }) {
+  return (
+    <Fragment>
+    <Head>
+      <title>Coding Anatomy</title>
+    </Head>
+    <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+    </Fragment>
+  );
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
